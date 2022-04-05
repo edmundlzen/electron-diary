6a3440912daeef6c2131c3dc966078e27219bcf5
Edmund <edmundlzen@gmail.com>
2022-04-05 17:36:24 +0800

    Added tailwind css

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -6,35 +6,9 @@ import 'tailwindcss/tailwind.css';
 const Hello = () => {
 	return (
 		<div>
-			<div className="Hello">
-				<img width="200px" alt="icon" src={icon} />
-			</div>
-			<h1>electron-react-boilerplate</h1>
-			<div className="Hello">
-				<a
-					href="https://electron-react-boilerplate.js.org/"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<button type="button">
-						<span role="img" aria-label="books">
-							📚
-						</span>
-						Read our docs
-					</button>
-				</a>
-				<a
-					href="https://github.com/sponsors/electron-react-boilerplate"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<button type="button">
-						<span role="img" aria-label="books">
-							🙏
-						</span>
-						Donate
-					</button>
-				</a>
+			<div className="flex flex-col items-center justify-center h-screen">
+				<img src={icon} alt="icon" className={'w-32 h-32'} />
+				<h1 className="text-center text-3xl font-bold">Hello World</h1>
 			</div>
 		</div>
 	);
