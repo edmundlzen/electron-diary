3246e385be63c729107514359bfd9c6ff0670a03
Edmund <edmundlzen@gmail.com>
2022-04-05 17:49:39 +0800

    Added Index page

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,24 +1,13 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
 import 'tailwindcss/tailwind.css';
-
-const Hello = () => {
-	return (
-		<div>
-			<div className="flex flex-col items-center justify-center h-screen">
-				<img src={icon} alt="icon" className={'w-32 h-32'} />
-				<h1 className="text-center text-3xl font-bold">Hello World</h1>
-			</div>
-		</div>
-	);
-};
+import Index from './pages';
 
 export default function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Hello />} />
+				<Route path="/" element={<Index />} />
 			</Routes>
 		</Router>
 	);
