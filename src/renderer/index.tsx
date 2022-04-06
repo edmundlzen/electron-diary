ceb095e63f3ee08283b472a9030b63c48d1bbb82
Edmund <edmundlzen@gmail.com>
2022-04-06 18:57:05 +0800

    Added native node modules support

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,9 +3,9 @@ import App from './App';
 
 render(<App />, document.getElementById('root'));
 
-// calling IPC exposed from preload script
-window.electron.ipcRenderer.once('ipc-example', (arg) => {
-	// eslint-disable-next-line no-console
-	console.log(arg);
-});
-window.electron.ipcRenderer.myPing();
+// // calling IPC exposed from preload script
+// window.electron.ipcRenderer.once('ipc-example', (arg) => {
+// 	// eslint-disable-next-line no-console
+// 	console.log(arg);
+// });
+// window.electron.ipcRenderer.myPing();
